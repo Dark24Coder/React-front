0b53265cd303c930821b3e9616836067ca6daeac
Francyl Koudanou <devartemis81@gmail.com>
2025-07-28 07:03:18 +0100

    Mon premier commit

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+// craco.config.js
+module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.ignoreWarnings = [
+        {
+          module: /stylis-plugin-rtl/,
+          message: /Failed to parse source map/,
+        },
+      ];
+      return webpackConfig;
+    },
+  },
+};
